69b79e957ab5fbef5d83190c1d8a46480e396c88
Jemarco Briz <jemarcobriz123@gmail.com>
2026-09-10 23:14:28 +0800

    feat(backend): connect the router to fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from app.api.routes.todos import router as todos_router
 
 app = FastAPI(
     title="Academic Task Manager API",
     version="0.1.0",
 )
 
+
+app,include_router(todos_router)
 
 @app.get("/health")
 def health_check():
